71d0f6447affd423571cf10359c814e31aa5f111
DebayanSen7 <debayan.sen7@gmail.com>
2016-04-03 07:41:10 +0530

    Updates for agsd1

---
+++ aboutGSD1/js/main.js
@@ -0,0 +1,20 @@
+$('.owl-carousel').owlCarousel({
+    items:1,
+    lazyLoad:true,
+    loop:true,
+    margin:10,
+    // autoplay:true,
+    // autoplayTimeout:3000,
+    autoplayHoverPause:false
+});
+$('.owl-carousel1').owlCarousel({
+    items:1,
+    lazyLoad:true,
+    loop:true,
+    margin:10,
+    mouseDrag:false,
+    touchDrag:false,
+    autoplay:true,
+    autoplayTimeout:3000,
+    autoplayHoverPause:false
+});
