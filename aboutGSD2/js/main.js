2a6064f1159adb3b48e1fb7ef89d254cc6f2e21b
DebayanSen7 <debayan.sen7@gmail.com>
2016-04-26 08:26:31 +0530

    files updated

--- aboutGSD2/js/main.js
+++ aboutGSD2/js/main.js
@@ -1,23 +1,37 @@
-$('.owl-carousel').owlCarousel({
-    items:1,
-    lazyLoad:true,
-    loop:true,
-    margin:10,
-    autoplay:true,
-    autoplayTimeout:3000,
-    autoplayHoverPause:false
+$(document).ready(function(){
+    $('.owl-carousel').owlCarousel({
+        items:1,
+        lazyLoad:true,
+        loop:true,
+        margin:10,
+        autoplay:true,
+        autoplayTimeout:3000,
+        autoplayHoverPause:false
+    });
+    $('.owl-carousel1').owlCarousel({
+        items:1,
+        lazyLoad:true,
+        loop:true,
+        margin:10,
+        mouseDrag:false,
+        touchDrag:false,
+        autoplay:true,
+        autoplayTimeout:3000,
+        autoplayHoverPause:false
+    });
+    $(function () {
+      $('[data-toggle="tooltip"]').tooltip();
+    })
+
+    // $('.message').delay(1000, function(){
+    //     $('.message').fadeIn('slow').show();
+    // });
+    //
+    //
+    // $('.closeBtn').off().on('click', function(){
+    //     $('.message').fadeOut( 1000, function() {
+    //
+    //     });
+    //
+    // });
 });
-$('.owl-carousel1').owlCarousel({
-    items:1,
-    lazyLoad:true,
-    loop:true,
-    margin:10,
-    mouseDrag:false,
-    touchDrag:false,
-    autoplay:true,
-    autoplayTimeout:3000,
-    autoplayHoverPause:false
-});
-$(function () {
-  $('[data-toggle="tooltip"]').tooltip();
-})
