7d9ad47b6a59335bf560950a659677a88639c7b7
debayansen <debayan.s@emamifrankross.com>
2016-05-17 17:24:59 +0530

    Updated new share buttons js

--- aboutGSD2/js/main.js
+++ aboutGSD2/js/main.js
@@ -24,13 +24,18 @@ $(document).ready(function(){
     })
 
     setTimeout(function(){
-        $('.message').fadeIn();
-    }, 2500);
+        $('.message, .likebox_area1').fadeIn();
+    }, 4000);
     $('.closeBtn').off().on('click', function(){
         $('.message').fadeOut(1000);
     });
 
-
-
+    // (function(d, s, id) {
+    //   var js, fjs = d.getElementsByTagName(s)[0];
+    //   if (d.getElementById(id)) return;
+    //   js = d.createElement(s); js.id = id;
+    //   js.src = "http://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.6";
+    //   fjs.parentNode.insertBefore(js, fjs);
+    // }(document, 'script', 'facebook-jssdk'));
 
 });
