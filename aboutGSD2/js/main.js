780c391064ea174f1b5b34c4c3164e090a20e07d
DebayanSen7 <debayan.sen7@gmail.com>
2016-05-18 08:45:48 +0530

    Updated css, js

--- aboutGSD2/js/main.js
+++ aboutGSD2/js/main.js
@@ -1,3 +1,8 @@
+/*!
+ Copyright of www.aboutgermanshepharddogs.com, 2016
+ Created by Debayan Sen, http://www.debayansen.com
+!*/
+
 $(document).ready(function(){
     $('.owl-carousel').owlCarousel({
         items:1,
@@ -24,18 +29,18 @@ $(document).ready(function(){
     })
 
     setTimeout(function(){
-        $('.message, .likebox_area1').fadeIn();
+        $('.message').fadeIn();
     }, 4000);
     $('.closeBtn').off().on('click', function(){
         $('.message').fadeOut(1000);
     });
 
-    // (function(d, s, id) {
-    //   var js, fjs = d.getElementsByTagName(s)[0];
-    //   if (d.getElementById(id)) return;
-    //   js = d.createElement(s); js.id = id;
-    //   js.src = "http://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.6";
-    //   fjs.parentNode.insertBefore(js, fjs);
-    // }(document, 'script', 'facebook-jssdk'));
+    (function(d, s, id) {
+      var js, fjs = d.getElementsByTagName(s)[0];
+      if (d.getElementById(id)) return;
+      js = d.createElement(s); js.id = id;
+      js.src = "http://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.6";
+      fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));
 
 });
